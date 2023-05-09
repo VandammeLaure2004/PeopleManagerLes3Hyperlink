f15e25f17cc70499bf9b504c7d2fc30b970d83e1
VandammeLaure2004 <laure.vandamme@student.vives.be>
2023-05-09 17:39:21 +0200

    Deel 7

--- PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -34,5 +34,37 @@ namespace PeopleManager.Ui.Mvc.Controllers
 
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var person = _database.People.Find(id);
+
+            if(person is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id,Person person)
+        {
+            var dbPerson = _database.People.Find(id);
+            if(dbPerson is null)
+            {
+                return RedirectToAction("index");
+            }
+            dbPerson.FirstName = person.FirstName;
+            dbPerson.LastName = person.LastName;
+            dbPerson.Email = person.Email;
+            dbPerson.Description = person.Description;
+
+            _database.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
